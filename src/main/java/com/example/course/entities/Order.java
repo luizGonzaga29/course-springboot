72a7cd205fae211c261153a74c1af089f49f4cf1
luizGonzaga29 <luizgonzagacabral29@gmail.comgit config --global user.email luizgonzagacabral29@gmail.com>
2022-03-30 14:13:04 -0300

    Subtotal & Total methods

--- src/main/java/com/example/course/entities/Order.java
+++ src/main/java/com/example/course/entities/Order.java
@@ -125,5 +125,14 @@ public class Order implements Serializable {
 			return false;
 		return true;
 	}
+	
+	public Double getTotal() {
+		
+		double total = 0.0;
+		for(OrderItem item : items) {
+			total+=item.getSubTotal();
+		}
+		return total;
+	}
 
 }
